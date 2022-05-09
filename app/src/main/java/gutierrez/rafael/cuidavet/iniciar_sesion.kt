e0b2323624f3c36f909dc1a9274002fcc8b0d980
arckenos <73735400+arckenos@users.noreply.github.com>
2022-05-09 10:23:36 -0700

    cosas

--- app/src/main/java/gutierrez/rafael/cuidavet/iniciar_sesion.kt
+++ app/src/main/java/gutierrez/rafael/cuidavet/iniciar_sesion.kt
@@ -4,31 +4,68 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import gutierrez.rafael.cuidavet.databinding.ActivityIniciarSesionBinding
 
 class iniciar_sesion : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityIniciarSesionBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_iniciar_sesion)
-        val btnIniciar : Button = findViewById(R.id.btnIniciar)
+        binding = ActivityIniciarSesionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val btnUsuarioNuevo : Button = findViewById(R.id.btnUsuarioNuevo)
 
-        btnIniciar.setOnClickListener {
-            var intent : Intent = Intent(this, MenuDesplegable::class.java)
-            startActivity(intent)
+        auth = Firebase.auth
+
+        binding.btnIniciar.setOnClickListener {
+            val mEmail = binding.etUsuario.text.toString()
+            val mPassw = binding.etContrasenia.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassw.isEmpty() ->{
+                    Toast.makeText(baseContext, "Mail o contrasenia incorrecta. ",
+                    Toast.LENGTH_SHORT).show()
+                }else->{
+                    signIn(mEmail, mPassw)
+                }
+            }
         }
 
 
-        btnUsuarioNuevo.setOnClickListener{
+        binding.btnUsuarioNuevo.setOnClickListener {
             var intent : Intent = Intent(this, registro::class.java)
             startActivity(intent)
         }
 
 
 
+    }
 
+    private fun signIn(email:String, password: String){
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task->
+                if(task.isSuccessful){
+                    Log.d("TAG", "Inicio de sesion con exito")
+                    reload();
+                }else{
+                    Log.d("TAG", "Fallo al iniciar sesion")
+                    Toast.makeText(baseContext, "Fallo al iniciar sesion",
+                        Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
 
+    private fun reload(){
+        val intent = Intent(this,MenuDesplegable::class.java)
+        this.startActivity(intent)
     }
 }
--- app/src/main/java/gutierrez/rafael/cuidavet/registro.kt
+++ app/src/main/java/gutierrez/rafael/cuidavet/registro.kt
@@ -3,23 +3,59 @@ package gutierrez.rafael.cuidavet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import gutierrez.rafael.cuidavet.databinding.ActivityRegistroBinding
 
 
 class registro : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
-        val anterior = iniciar_sesion::class.java
-        val btnSiguiente : Button = findViewById(R.id.btnSiguiente)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnSiguiente.setOnClickListener {
-            var intent : Intent = Intent(this, MenuDesplegable::class.java)
-            startActivity(intent)
+        auth = Firebase.auth
+
+        binding.btnSiguiente.setOnClickListener {
+            val email = binding.etCorreo.text.toString()
+            val user = binding.etUsuario.text.toString()
+            val nombre = binding.etNombre.text.toString()
+            val password = binding.etContrasenia.text.toString()
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(baseContext,"Ingrese todos los datos", Toast.LENGTH_SHORT).show()
+            }else if(password.length < 6){
+                Toast.makeText(baseContext,"Contrasenia muy corta", Toast.LENGTH_SHORT).show()
+            }else{
+                createAccount(email,password)
+            }
+
         }
 
+    }
 
+    private fun createAccount(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    Toast.makeText(baseContext,"Usuario registado con exito", Toast.LENGTH_SHORT).show()
+                    reload()
+                }else{
+                    Toast.makeText(baseContext,"Autentication failed", Toast.LENGTH_SHORT).show()
+                }
 
+            }
+    }
+
+    private fun reload(){
+        val intent : Intent = Intent(this, iniciar_sesion::class.java)
+        startActivity(intent)
     }
 }
